87411260a509e462f3743b604afa3b4c5b846d7d
Victor Fortunato <lakotaclip@gmail.com>
2022-09-15 08:16:30 -0600

    First commit

---
+++ src/Patterns.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+public class Patterns {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		for(int i = 1; i <= 1; i++) {
+			for(int j = 0; j < i; j++) {
+			
+			System.out.println("    j" + "    a" + "   v" + "     v" + "   a");
+		
+		
+			}
+		}
+		
+			for(int i = 0; i <= 0; i++) {
+				for(int j = 0; j < i; j++) {
+				}
+			
+				System.out.println("    j" + "   a a" + "   v   v"  + "   a a");	
+		
+		
+			
+			}
+			
+			for(int i = 0; i <= 0; i++) {
+				for(int j = 0; j < i; j++) {
+				}
+			
+				System.out.println(" j  j" + "  aaaaa" + "   v v"  + "   aaaaa");	
+		
+		
+			
+			}
+			
+			for(int i = 0; i <= 0; i++) {
+				for(int j = 0; j < i; j++) {
+				}
+			
+				System.out.println("  jj" + "  a     a" + "   v"  + "   a     a");	
+		
+		
+			
+			}
+	}
+}
+
